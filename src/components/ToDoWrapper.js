ca52efe8b581c7ef8d4b545e0007040ee5eaf6d2
nivin2004 <115636503+nivin2004@users.noreply.github.com>
2024-07-05 13:24:33 +0530

    Add files via upload

---
+++ src/components/ToDoWrapper.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Todo } from "./ToDo";
+import { TodoForm } from "./ToDoForm";
+import { EditTodoForm } from "./EditTodoForm";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const generateId = () => {
+    return Date.now() + Math.random().toString(36).substr(2, 9);
+  };
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: generateId(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm key={todo.id} editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
